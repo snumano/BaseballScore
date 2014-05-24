8f418b8474015db3319068a2316493acf34e2183
Shugo Numano <numano@cc.rim.or.jp>
2014-05-24 16:57:41 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Baseball Score'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Baseball Score')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Baseball Score | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Baseball Score | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Baseball Score | About Us")
+    end
+  end
+end
