cb2d92be3ad8876ea99b419cfa2bf1faeb6cf94f
Shugo Numano <numano@cc.rim.or.jp>
2014-05-28 06:14:28 +0900

    revise spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,9 +29,7 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        valid_signin(user)
       end
 
       it { should have_title(user.name) }
