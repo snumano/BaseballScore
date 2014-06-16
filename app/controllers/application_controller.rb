da64e5618da2374d3c1afcba0153792b97f96b77
Shugo Numano <numano@cc.rim.or.jp>
2014-06-17 07:40:09 +0900

    introduce i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 #  include SessionsHelper
-
   before_filter :configure_devise_params, if: :devise_controller?
+  before_filter :set_locale
 
   protected
     def configure_devise_params
@@ -12,5 +12,15 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
 
     end
+
+    def set_locale
+      locale = params[:locale] || locale_from_accept_language
+      I18n.locale = (I18n::available_locales.include? locale.to_sym) ? locale.to_sym : I18n.default_locale
+    end
+ 
+    def locale_from_accept_language 
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
+
 
 end
